d0d072c59f335c078e4fdb94f4698e605542d81d
Braeden <braeden.norman6@gmail.com>
2023-02-05 16:42:21 -0800

    made user entity password deferred, so to not return unless directly requested

--- core/models/user.py
+++ core/models/user.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from core.models.database import Base
+from sqlalchemy.orm import deferred
 
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String)
-    password = Column(String)
+    password = deferred(Column(String))
     first_name = Column(String)
     last_name = Column(String)
     middle_name = Column(String)
