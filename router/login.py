c9d584a7e46330867bd9c00880b5b4859ff1c2d7
Braeden <braeden.norman6@gmail.com>
2023-02-11 13:20:20 -0800

    fixed login and registration to salt and hash passwords in database

--- router/login.py
+++ router/login.py
@@ -12,12 +12,6 @@ from jose import jwt
 SECRET = "asdjsdvhb2qjkb4e32jkfbasbkn324"
 ALGORITHM = "HS256"
 
-
-SALT_LENGTH = 4
-
-# the recent given salt for each ip
-salt = {}
-
 Base.metadata.create_all(bind=engine)
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
@@ -41,9 +35,6 @@ def create_token(id: int, full_name: str):
     return jwt.encode(encode, SECRET, algorithm=ALGORITHM)
 
 
-def generateRandomString(length: str):
-    return ''.join(random.choice(string.ascii_letters) for _ in range(length))
-
 async def verify_token(token: str = Depends(OAuth2PasswordBearer(tokenUrl="/login"))):
     try:
         payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
@@ -54,28 +45,15 @@ async def verify_token(token: str = Depends(OAuth2PasswordBearer(tokenUrl="/logi
         raise HTTPException(status_code=401, detail="Invalid token")
 
 
-# https://stackoverflow.com/questions/3715920/is-it-worth-hashing-passwords-on-the-client-side
-@router.get("/")
-async def login(request: Request):
-    global salt
-    salt[request.client.host] = generateRandomString(SALT_LENGTH)
-    return {"salt": salt[request.client.host]}
-
-
 @router.post("/")
-async def login(request: Request, clientSalt: str, form_data: OAuth2PasswordRequestForm = Depends(),  db: Session = Depends(get_db),):
-    global salt
-    if salt.get(request.client.host) == None:
-        raise HTTPException(
-            status_code=400, detail="no salt requested from this ip")
-
+async def login(request: Request, form_data: OAuth2PasswordRequestForm = Depends(),  db: Session = Depends(get_db),):
     user = db.query(User).filter(User.email == form_data.username).first()
     if user == None:
         raise HTTPException(status_code=404, detail="user not found")
 
-    finalPass = str(hashlib.sha256(
-        (salt[request.client.host] + user.password + clientSalt).encode('utf-8')).hexdigest())
-    if finalPass != form_data.password:
+    hashedPass = str(hashlib.sha256((form_data.password + user.salt).encode()).hexdigest())
+
+    if hashedPass != user.password:
         raise HTTPException(status_code=401, detail="password incorrect")
 
     # update last login
--- router/registration.py
+++ router/registration.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 from typing import Optional
 from core.models.database import engine, SessionLocal, Base
 from sqlalchemy.orm import Session
+import bcrypt
+import hashlib
 
 from core.models.user import User
 
@@ -32,16 +34,24 @@ class NewUser(BaseModel):
     phone_number: str
 
 
+# TODO: check email is valid, check phone number is valid, some sort of check for naming (too long, too short, etc)
 @router.post("/")
 async def registration(request: Request, newUser: NewUser, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == newUser.email).first()
     if user != None:
         raise HTTPException(status_code=400, detail="User already exists with that email")
 
+    user = db.query(User).filter(User.phone_number == newUser.phone_number).first()
+    if user != None:
+        raise HTTPException(status_code=400, detail="User already exists with that phone number")
+
+    salt = bcrypt.gensalt().hex()
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    user = User(email=newUser.email, password=newUser.password, first_name=newUser.first_name,
-           last_name=newUser.last_name, middle_name=newUser.middle_name, phone_number=newUser.phone_number, created= timestamp, last_login=timestamp, role_id='user', active=True)
+
+    hashedPass = str(hashlib.sha256((newUser.password + salt).encode()).hexdigest())
+    user = User(email=newUser.email, password= hashedPass, first_name=newUser.first_name,
+           last_name=newUser.last_name, middle_name=newUser.middle_name, phone_number=newUser.phone_number, 
+           created= timestamp, last_login=timestamp, role_id='user', active=True, version = 0, salt = salt)
     db.add(user)
     db.commit()
-    
     return  {"first_name": user.first_name, "last_name": user.last_name, "email": user.email, "role": user.role_id}
