46be9eeddd46e9959856d4026c33cafe6543a11f
Braeden <braeden.norman6@gmail.com>
2023-02-11 13:19:52 -0800

    updated SQL and user entity to match

--- core/models/user.py
+++ core/models/user.py
@@ -1,17 +1,20 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, BigInteger, VARCHAR, TIMESTAMP, Enum
 from core.models.database import Base
 from sqlalchemy.orm import deferred
 
 class User(Base):
     __tablename__ = "user"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String)
-    password = deferred(Column(String))
-    first_name = Column(String)
-    last_name = Column(String)
-    middle_name = Column(String)
-    phone_number = Column(String)
-    created = Column(String)
-    last_login = Column(String)
-    role_id = Column(String)
-    active = Column(Boolean)
+    id = Column(BigInteger, primary_key=True, index=True)
+    email = Column(VARCHAR(45), nullable=False, unique=True)
+    password = deferred(Column(VARCHAR(70), nullable=False))
+    first_name = Column(VARCHAR(45), nullable=False)
+    last_name = Column(VARCHAR(45), nullable=False)
+    middle_name = Column(VARCHAR(45), nullable=True)
+    phone_number = Column(VARCHAR(45), nullable=False)
+    created = Column(TIMESTAMP, nullable=False)
+    updated = Column(TIMESTAMP, nullable=True)
+    last_login = Column(TIMESTAMP, nullable=False)
+    role_id = Column(Enum('user', 'admin'), nullable=False)
+    active = Column(Boolean, nullable=False)
+    version = Column(Integer, nullable=False)
+    salt = Column(VARCHAR(32), nullable=False)
