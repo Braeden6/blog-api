004bf38ed28bc2522d8232f1d72f9e2465f017f9
Braeden <braeden.norman6@gmail.com>
2023-02-15 11:17:19 -0800

    changed ACCESS TOKEN TIMER to be a global variable

--- router/login.py
+++ router/login.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timedelta
 from fastapi import Request, HTTPException, Depends, APIRouter
-import random
-import string
 import hashlib
 from core.models.database import engine, SessionLocal, Base
 from sqlalchemy.orm import Session
@@ -11,10 +9,12 @@ from jose import jwt
 
 SECRET = "asdjsdvhb2qjkb4e32jkfbasbkn324"
 ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 Base.metadata.create_all(bind=engine)
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+
 
 router = APIRouter(
     prefix="/login",
@@ -31,7 +31,7 @@ def get_db():
 
 def create_token(id: int, full_name: str):
     encode = {"sub": full_name, "id": id,
-              "exp": datetime.utcnow() + timedelta(minutes=60)}
+              "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)}
     return jwt.encode(encode, SECRET, algorithm=ALGORITHM)
 
 
@@ -52,7 +52,6 @@ async def login(request: Request, form_data: OAuth2PasswordRequestForm = Depends
         raise HTTPException(status_code=404, detail="user not found")
 
     hashedPass = str(hashlib.sha256((form_data.password + user.salt).encode()).hexdigest())
-
     if hashedPass != user.password:
         raise HTTPException(status_code=401, detail="password incorrect")
 
