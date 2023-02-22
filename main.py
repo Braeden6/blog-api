f5ab9898238eb52bb0a774dba14c888b0c50e981
Braeden <braeden.norman6@gmail.com>
2023-02-21 19:13:30 -0800

    removed graphql and added nofitications to main router

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from core.models.database import engine, SessionLocal, Base
 from sqlalchemy.orm import Session
 from core.models.user import User
-from router import login, registration, post, answer, comment, votes,admin
+from router import login, registration, post, answer, comment, votes, admin, notifications
 from strawberry.fastapi import GraphQLRouter
 from strawberry.types import Info
 import strawberry
@@ -90,27 +90,6 @@ async def log_requests(request: Request, call_next, ):
     db.commit()
     return response
 
-async def notify_new_flavour(name: str):
-    print(name)
-
-
-@strawberry.type
-class Query:
-    @strawberry.field
-    def hello(self) -> str:
-        return "Hello World"
-
-
-@strawberry.type
-class Mutation:
-    @strawberry.mutation
-    def create_flavour(self, name: str, info: Info) -> bool:
-        info.context["background_tasks"].add_task(notify_new_flavour, name)
-        return True
-
-
-schema = strawberry.Schema(query=Query)
-graphql_router = GraphQLRouter(schema)
 
 Base.metadata.create_all(bind=engine)
 
@@ -120,18 +99,17 @@ app.include_router(post.router)
 app.include_router(comment.router)
 app.include_router(answer.router)
 app.include_router(votes.router)
-app.include_router(admin.router)
-app.include_router(graphql_router, prefix="/graphql")
+# app.include_router(admin.router)
+app.include_router(notifications.router)
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.get("/users")
-async def read_all(db: Session = Depends(get_db)):
-    return db.query(User).all()
+# @app.get("/users")
+# async def read_all(db: Session = Depends(get_db)):
+#     return db.query(User).all()
